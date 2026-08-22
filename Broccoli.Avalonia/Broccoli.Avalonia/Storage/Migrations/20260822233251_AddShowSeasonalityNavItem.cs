857ab7a27a525fbddf7c11ca8ea78b941e0d0027
Tristan Kells <tristankells@gmaill.com>
2026-08-23 11:33:20 +1200

    Add ShowSeasonalityNavItem setting to show/hide the Seasonality tab

---
+++ Broccoli.Avalonia/Broccoli.Avalonia/Storage/Migrations/20260822233251_AddShowSeasonalityNavItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Broccoli.Avalonia.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowSeasonalityNavItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowSeasonalityNavItem",
+                table: "MacroTargetSettings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowSeasonalityNavItem",
+                table: "MacroTargetSettings");
+        }
+    }
+}

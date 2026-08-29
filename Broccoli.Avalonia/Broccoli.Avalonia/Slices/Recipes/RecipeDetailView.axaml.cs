0ee05b6ebb381754e67ae7afca377681fc21c3b3
Tristan Kells <tristankells@gmaill.com>
2026-08-29 13:49:35 +1200

    Fade in recipe detail images and add groceries loading spinner

--- Broccoli.Avalonia/Broccoli.Avalonia/Slices/Recipes/RecipeDetailView.axaml.cs
+++ Broccoli.Avalonia/Broccoli.Avalonia/Slices/Recipes/RecipeDetailView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Broccoli.Avalonia.Slices.Recipes;
 
@@ -7,5 +8,17 @@ public partial class RecipeDetailView : UserControl
     public RecipeDetailView()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Fades an image thumbnail in from <c>Opacity="0"</c> once it is realized, so pictures
+    /// don't pop in abruptly when the page appears.
+    /// </summary>
+    private void OnImageLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Image image)
+        {
+            image.Opacity = 1;
+        }
     }
 }
